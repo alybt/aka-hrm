3a55ca42e7caf5b8919edef39841456d90ec87b4
alybt <alelybitun.school@gmail.com>
2026-04-01 10:10:59 +0800

    add: Custom Add-ons: hr-holidays-extenstion: Add Menu in Views and Manifest

---
+++ custom-addons/hr_holiday_public_extension/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "HR Holidays Public Extension",
+    "depends": [
+        "hr_holidays",
+        "hr_holidays_public",
+    ],
+    "data": [
+        "views/hr_holidays_public_views.xml",
+    ],
+    "installable": True,
+}
