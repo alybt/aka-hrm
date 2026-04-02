c9cda42a860925a76393a1c6e64665a305fab980
alybt <alelybitun.school@gmail.com>
2026-04-02 17:03:24 +0800

    add: Public Holiday: Holiday Type

--- custom-addons/ph_holiday_types/__manifest__.py
+++ custom-addons/ph_holiday_types/__manifest__.py
@@ -1,19 +1,13 @@
 {
-    'name': 'Philippines Holiday Types Extension',
+    'name': 'Philippines Public Holiday Types',
     'version': '1.0',
     'category': 'Human Resources',
-    'summary': 'Adds Regular, Special, and Local holiday types for PH Payroll.',
-    "license": "AGPL-3",
-    'depends': [
-        'resource', 
-        'hr_work_entry', 
-        'hr_holidays'],
+    'summary': 'Categorize Public Holidays as Regular, Special, or Local.',
+    'depends': ['base', 'resource', 'hr_holidays'],
     'data': [
-        'data/hr_work_entry_type_data.xml',
-        # 'views/resource_calendar_leaves_views.xml',
+        'views/resource_calendar_leaves_views.xml',
     ],
-    "maintainers": ["nimarosa"],
-    "installable": True,
-    "application": True,
-    "auto_install": True,
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
 }
--- custom-addons/ph_holiday_types/models/resource_calendar_leaves.py
+++ custom-addons/ph_holiday_types/models/resource_calendar_leaves.py
@@ -1,15 +1,11 @@
 from odoo import fields, models
 
-class ResourceCalendarLeave(models.Model):
+class ResourceCalendarLeaves(models.Model):
     _inherit = 'resource.calendar.leaves'
 
     holiday_type = fields.Selection([
         ('regular', 'Regular Holiday'),
-        ('special_non_working', 'Special Non-Working Holiday'),
-        ('special_working', 'Special Working Holiday'),
+        ('special_non_working', 'Special Non-Working Day'),
+        ('special_working', 'Special Working Day'),
         ('local', 'Local Holiday')
-    ], 
-        string = 'PH Holiday Type',
-        default = 'regular',
-        help = "Categorization for Philippine Labor Law Compliance."
-    )
+    ], string='PH Holiday Type', default='regular', help="Select the type of holiday for Philippine Payroll.")
