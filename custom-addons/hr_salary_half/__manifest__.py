c3df113d30e2b0183dadb733915ee7f1140659dd
alybt <alelybitun.school@gmail.com>
2026-04-03 01:35:00 +0800

    add: Custom Add-ons: HR Salary Half Module

---
+++ custom-addons/hr_salary_half/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Half of the Salary',
+    'version': '1.0',
+    'category': 'Human Resources',
+    'summary': 'Half of the Contract Monthly Salary',
+    'depends': ['payroll'],
+    'data': [
+        'views/hr_payslip_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+    "auto_install": True,
+    'license': 'LGPL-3',
+}
