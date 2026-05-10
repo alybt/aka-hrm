4f67077cefbc9f7373666ab36d26cf3657e9f0ad
alybt <alelybitun.school@gmail.com>
2026-05-10 21:15:29 +0800

    add: /salary_computation Time Bank for the Excess Worked Hours

--- custom-addons/salary_computation/__manifest__.py
+++ custom-addons/salary_computation/__manifest__.py
@@ -2,9 +2,10 @@
     'name': 'Salary Computation',
     'version': '1.0',
     'category': 'Human Resources',
-    'depends': ['payroll'],
+    'depends': ['hr', 'payroll'],
     'data': [
         'views/hr_payslip_view.xml',
+        'views/time_bank.xml',
     ],
     'installable': True,
     'application': True,
