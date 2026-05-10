e9b7263089b1eed49a09feeb63bbc4198de96e17
alybt <alelybitun.school@gmail.com>
2026-05-10 20:54:35 +0800

    add: /salary_computation, From Working days, Daily Rate, Half A Month, Regular Days

---
+++ custom-addons/salary_computation/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Salary Computation',
+    'version': '1.0',
+    'category': 'Human Resources',
+    'depends': ['payroll'],
+    'data': [
+        'views/hr_payslip_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+    "auto_install": False,
+    'license': 'LGPL-3',
+}
